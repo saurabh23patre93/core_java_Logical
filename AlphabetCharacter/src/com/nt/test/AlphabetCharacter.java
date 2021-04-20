3386817e427e21dd6b24ad43c4088da28ba41e9b
asus <asus@DESKTOP-5V94U0C-Saurabh>
2021-04-20 14:53:47 +0530

    Alphabet character program

---
+++ AlphabetCharacter/src/com/nt/test/AlphabetCharacter.java
@@ -0,0 +1,14 @@
+package com.nt.test;
+
+public class AlphabetCharacter {
+
+	public static void main(String[] args) {
+		char c='*';
+		
+		if((c>='a'&& c<='z') ||(c>='A'&& c<='Z'))
+			System.out.println(c+" is an alphabet");
+		else
+			System.out.println(c+" is not an alphabet");
+	}
+
+}
