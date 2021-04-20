713258302ae26a6ed3c491ac4e60646853c2366d
asus <asus@DESKTOP-5V94U0C-Saurabh>
2021-04-20 14:53:13 +0530

    distence btw 2 point program

---
+++ DistenceBwn2Point/src/com/nt/distence/DistanceBwPoint.java
@@ -0,0 +1,16 @@
+package com.nt.distence;
+
+public class DistanceBwPoint {
+
+	public static void main(String[] args) {
+		int x1,x2,y1,y2;
+		double distance;
+		x1=1;y1=1;
+		x2=4;y2=4;
+		
+		distance=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		System.out.println("Distance Between "+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+distance);
+
+	}
+
+}
